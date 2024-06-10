dd29b2d36c1bce6b8e47b960007fb9b4552a3b50
Vitaly <onebeattrue@gmail.com>
2024-06-10 13:30:33 +0300

    Написал EventRepository

---
+++ src/main/kotlin/edu/mirea/onebeattrue/database/event/EventRepository.kt
@@ -0,0 +1,70 @@
+package edu.mirea.onebeattrue.database.event
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object EventRepository {
+
+    fun createEvent(event: EventDto): Int {
+        val insertStatement = transaction {
+            Events.insert {
+                it[time] = event.time
+                it[label] = event.label
+                it[repeatable] = event.repeatable
+                it[petId] = event.petId
+            }
+        }
+
+        return insertStatement.resultedValues?.first()?.get(Events.id)
+            ?: throw IllegalStateException("Failed to insert Event")
+    }
+
+    fun getEventById(id: Int): EventDto? {
+        return transaction {
+            Events.selectAll().where { Events.id eq id }
+                .map {
+                    EventDto(
+                        id = it[Events.id],
+                        time = it[Events.time],
+                        label = it[Events.label],
+                        repeatable = it[Events.repeatable],
+                        petId = it[Events.petId]
+                    )
+                }
+                .singleOrNull()
+        }
+    }
+
+    fun updateEvent(event: EventDto): Boolean {
+        return transaction {
+            Events.update({ Events.id eq event.id }) {
+                it[time] = event.time
+                it[label] = event.label
+                it[repeatable] = event.repeatable
+                it[petId] = event.petId
+            } > 0
+        }
+    }
+
+    fun deleteEvent(id: Int): Boolean {
+        return transaction {
+            Events.deleteWhere { Events.id eq id } > 0
+        }
+    }
+
+    fun getEventsByPetId(petId: Int): List<EventDto> {
+        return transaction {
+            Events.select { Events.petId eq petId }
+                .map {
+                    EventDto(
+                        id = it[Events.id],
+                        time = it[Events.time],
+                        label = it[Events.label],
+                        repeatable = it[Events.repeatable],
+                        petId = it[Events.petId]
+                    )
+                }
+        }
+    }
+}
