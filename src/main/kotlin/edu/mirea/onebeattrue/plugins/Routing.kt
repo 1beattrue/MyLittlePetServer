210ca1a6d6fe76c4824a42dba56dc59ac8e7c894
Vitaly <onebeattrue@gmail.com>
2024-06-13 23:13:08 +0300

    Поправил route

--- src/main/kotlin/edu/mirea/onebeattrue/plugins/Routing.kt
+++ src/main/kotlin/edu/mirea/onebeattrue/plugins/Routing.kt
@@ -15,42 +15,44 @@ fun Application.configureRouting() {
             call.respond("My Little Pet Server")
         }
 
-        route("/users") {
-            post("/create") { UserController().createUser(call) }
-            get("/{token}") { UserController().getUserByToken(call) }
-            delete("/{token}") { UserController().deleteUser(call) }
-        }
+        route("/api/v1") {
+            route("/users") {
+                post("/create") { UserController().createUser(call) }
+                get("/{token}") { UserController().getUserByToken(call) }
+                delete("/{token}") { UserController().deleteUser(call) }
+            }
 
-        route("/pets") {
-            post("/create") { PetController().createPet(call) }
-            get("/{id}") { PetController().getPetById(call) }
-            put("/{id}") { PetController().updatePet(call) }
-            delete("/{id}") { PetController().deletePet(call) }
-            get("/byUserToken/{userToken}") { PetController().getPetsByUserToken(call) }
-        }
+            route("/pets") {
+                post("/create") { PetController().createPet(call) }
+                get("/{id}") { PetController().getPetById(call) }
+                put("/{id}") { PetController().updatePet(call) }
+                delete("/{id}") { PetController().deletePet(call) }
+                get("/byUserToken/{userToken}") { PetController().getPetsByUserToken(call) }
+            }
 
-        route("/notes") {
-            post("/create") { NoteController().createNote(call) }
-            get("/{id}") { NoteController().getNoteById(call) }
-            put("/{id}") { NoteController().updateNote(call) }
-            delete("/{id}") { NoteController().deleteNote(call) }
-            get("/byPetId/{petId}") { NoteController().getNotesByPetId(call) }
-        }
+            route("/notes") {
+                post("/create") { NoteController().createNote(call) }
+                get("/{id}") { NoteController().getNoteById(call) }
+                put("/{id}") { NoteController().updateNote(call) }
+                delete("/{id}") { NoteController().deleteNote(call) }
+                get("/byPetId/{petId}") { NoteController().getNotesByPetId(call) }
+            }
 
-        route("/medicalData") {
-            post("/create") { MedicalDataController().createMedicalData(call) }
-            get("/{id}") { MedicalDataController().getMedicalDataById(call) }
-            put("/{id}") { MedicalDataController().updateMedicalData(call) }
-            delete("/{id}") { MedicalDataController().deleteMedicalData(call) }
-            get("/byPetId/{petId}") { MedicalDataController().getMedicalDataByPetId(call) }
-        }
+            route("/medicalData") {
+                post("/create") { MedicalDataController().createMedicalData(call) }
+                get("/{id}") { MedicalDataController().getMedicalDataById(call) }
+                put("/{id}") { MedicalDataController().updateMedicalData(call) }
+                delete("/{id}") { MedicalDataController().deleteMedicalData(call) }
+                get("/byPetId/{petId}") { MedicalDataController().getMedicalDataByPetId(call) }
+            }
 
-        route("/events") {
-            post("/create") { EventController().createEvent(call) }
-            get("/{id}") { EventController().getEventById(call) }
-            put("/{id}") { EventController().updateEvent(call) }
-            delete("/{id}") { EventController().deleteEvent(call) }
-            get("/byPetId/{petId}") { EventController().getEventsByPetId(call) }
+            route("/events") {
+                post("/create") { EventController().createEvent(call) }
+                get("/{id}") { EventController().getEventById(call) }
+                put("/{id}") { EventController().updateEvent(call) }
+                delete("/{id}") { EventController().deleteEvent(call) }
+                get("/byPetId/{petId}") { EventController().getEventsByPetId(call) }
+            }
         }
     }
 }
